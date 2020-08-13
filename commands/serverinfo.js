64d083491cece4f053bdaed024b903ddbfd51cba
Ksdsk <48192419+Ksdsk@users.noreply.github.com>
2020-08-13 20:18:47 -0300

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -1,16 +1,29 @@
 const Discord = require('discord.js');
+const invite = require('./invite');
 
 module.exports = {
     name: 'serverinfo',
     description: 'This command sends all information about this server.',
     execute(message, args){
 
+
+        message.channel.createInvite({
+            "maxAge": 8640000
+        }).then(invite => {
+            var invCode = `https://discord.gg/invite/${invite.code}`;
+            return invCode;
+        }
+        ).catch(console.error);
+        
+        
+
+
         const serverInfoEmbed = new Discord.MessageEmbed()
         .setTitle(`Information for ${message.guild.name}`)
         .setDescription(`Guild description: ${message.guild.description}`)
         .addFields(
             {name: "Created on:", value: `${message.guild.createdAt}`},
-            {name: "Invite link: ", value: `https://discord.gg/invite${message.channel.createInvite().code}`},
+            {name: "Invite link: ", value: `https://discord.gg/invite${invite()}`},
             {name: "Guild ID: ", value: `${message.guild.id}`},
             {name: "Server owner: ", value: `${message.guild.owner}`},
             {name: "Region: ", value: `${message.guild.region}`},
