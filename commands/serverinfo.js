83bf068786a620d852a077cb6be4a03e7fc145e4
Ksdsk <48192419+Ksdsk@users.noreply.github.com>
2020-08-13 18:41:29 -0300

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -9,12 +9,12 @@ module.exports = {
         .setTitle(`Information for ${message.guild.name}`)
         .setDescription(`Guild description: ${message.guild.description}`)
         .addFields(
-            {name: "Created on:", value: `${message.guild.createdAt}`, inline: true},
-            {name: "Invite link: ", value: `https://discord.gg/invite${message.channel.createInvite().code}`, inline: true},
-            {name: "Guild ID: ", value: `${message.guild.id}`, inline: true},
-            {name: "Server owner: ", value: `${message.guild.owner}`, inline: true},
-            {name: "Region: ", value: `${message.guild.region}`, inline: true},
-            {name: "Number of members:", value: `${message.guild.memberCount}`},
+            {name: "Created on:", value: `${message.guild.createdAt}`},
+            {name: "Invite link: ", value: `https://discord.gg/invite${message.channel.createInvite().code}`},
+            {name: "Guild ID: ", value: `${message.guild.id}`},
+            {name: "Server owner: ", value: `${message.guild.owner}`},
+            {name: "Region: ", value: `${message.guild.region}`},
+            {name: "Number of members:", value: `${message.guild.memberCount}`}
         )
 
 
