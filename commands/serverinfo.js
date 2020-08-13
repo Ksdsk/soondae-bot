f8c10ce49331290a7ce7548ef50124ccc3bbc30c
Ksdsk <48192419+Ksdsk@users.noreply.github.com>
2020-08-13 18:39:19 -0300

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -5,16 +5,6 @@ module.exports = {
     description: 'This command sends all information about this server.',
     execute(message, args){
 
-        message.channel.createInvite({
-            "maxAge": time
-        }).then(invite => message.channel.send({
-            "embed": {
-                'color': '#7aff81',
-                'title': `Invite link for ${message.channel.name}`,
-                'description': `https://discord.gg/invite/${invite.code}`
-            }
-        })).catch(console.error);
-
         const serverInfoEmbed = new Discord.MessageEmbed()
         .setTitle(`Information for ${message.guild.name}`)
         .setDescription(`Guild description: ${message.guild.description}`)
