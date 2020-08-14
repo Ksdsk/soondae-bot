beb7c6ebbd8a619602e94668634a3799254b5cb7
Ksdsk <48192419+Ksdsk@users.noreply.github.com>
2020-08-13 22:06:38 -0300

    Server Info V 1

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const invite = require('./invite');
-
+const moment = require('moment');
 module.exports = {
     name: 'serverinfo',
     description: 'This command sends all information about this server.',
@@ -31,23 +31,43 @@ module.exports = {
 
 
         .addField('General', [
-            `**Created on: ** ${message.guild.createdAt}`,
-            `**Invite link: **${invite}`,
-            `**Guild ID: **${message.guild.id}`,
-            `**Server owner: **${message.guild.owner.user.tag}`,
-            `**Server region: **${message.guild.region}`
+            `**Created on: ** ${moment(message.guild.createdAt).format('LT')} ${moment(message.guild.createdAt).format('LL')} ${moment(message.guild.createdAt).fromNow()}\n`,
+            `**Invite Link: **${invite}\n`,
+            `**Guild ID: **${message.guild.id}\n`,
+            `**Server Owner: **${message.guild.owner.user.tag}\n`,
+            `**Server Region: **${message.guild.region}\n`,
+            `**Boost Tier : **${message.guild.premiumTier ? `Tier ${message.guild.premiumTier}` : "None"}\n`,
+            `**Explicit Filter: **${message.guild.explicitContentFilter}\n`,
+            `**Verification level: **${message.guild.verificationLevel}\n`,
+            `**Server region: **${message.guild.region}\n`,
+
         ])
 
 
         .addField('Members', [
-
-            {name: "Number of members:", value: `${message.guild.memberCount}`},
-            {name: "Online: ", value: `${message.guild.region}`}
+            `**Total: **${message.guild.memberCount}\n`,
+            `**Online: **${members.filter(member => member.presence.status === 'online')}\n`,
+            `**Away: **${members.filter(member => member.presence.status === 'idle')}\n`,
+            `**Do Not Disturb: **${members.filter(member => member.presence.status === 'dnd')}\n`,
+            `**Offline: **${members.filter(member => member.presence.status === 'offline')}\n`,
+            `**Humans: **${members.filter(member => !member.user.bot).size}\n`,
+            `**Bots: **${members.filter(member => member.user.bot).size}\n`,
         ])
 
 
 
+        .addField('Numbers', [
+            `**Number of Roles: **${roles.length}\n`,
+            `**Number of All Emojis: **${emojis.size}\n`
+            `**Number of Regular Emojis: **${emojis.filter(emoji => !emoji.animated).size}\n`,
+            `**Number of Animated Emojis: **${emojis.filter(emoji => emoji.animated).size}\n`,
+            `**Number of Text Channels: **${channels.filter(channel => channel.type === 'text').size}\n`,
+            `**Number of Text Channels: **${channels.filter(channel => channel.type === 'voice').size}\n`,
+            `**Number of Boosts : **${message.guild.premiumSubscriptionCount || '0'}\n`,
+        ])
 
+        .addField('Roles', [`${roles.length - 1}`], roles.length < 10 ? roles.join(', ') : roles.length > 10 ? this.client.utils.trimArray(roles) : "None")
+        .setTimestamp()
 
         
         message.channel.send(serverInfoEmbed);
