0dbdd74cbe51be460f7a0a8d30cf06a6caf30abe
Ksdsk <48192419+Ksdsk@users.noreply.github.com>
2020-09-02 16:24:54 -0300

    Update socialmedia.js

--- commands/socialmedia.js
+++ commands/socialmedia.js
@@ -63,7 +63,7 @@ module.exports = {
 
 
 
-        async function socialMedia() {
+        function socialMedia() {
             const instaReq = new Discord.MessageEmbed()
             .setTimestamp()
             .setColor('#4af3ff')
