01e1db93f3a23a88f9bdb7e1052e6453e15090a4
Ksdsk <48192419+Ksdsk@users.noreply.github.com>
2020-09-02 19:55:40 -0300

    Update socialmedia.js

--- commands/socialmedia.js
+++ commands/socialmedia.js
@@ -8,16 +8,18 @@ module.exports = {
     description: 'This command uploads social media of the user given each argument',
     execute(message, args){
                     
-                    message.channel.send("Enter your name");
-                    let filter = m => m.author.id === message.author.id;
-                    try {
-                        let msg = await message.channel.awaitMessages(filter, { maxMatches: 1, time: '10000', errors: ['time'] });
-                        message.channel.send("Your name " + msg.first().content);
-                    }
-                    catch(ex) {
-                        message.channel.send("You did not specify a name on time.");
-                    }
 
-
+            async function firstSecon() {
+                message.channel.send("Enter your name");
+                let filter = m => m.author.id === message.author.id;
+                try {
+                    let msg = await message.channel.awaitMessages(filter, { maxMatches: 1, time: '10000', errors: ['time'] });
+                    message.channel.send("Your name " + msg.first().content);
+                }
+                catch(ex) {
+                    message.channel.send("You did not specify a name on time.");
+                }
             }
+            firstSecon();
+        }
 }
