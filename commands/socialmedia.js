bff30146487d1df39e1d23df8b64cc4438ddcfb3
Ksdsk <48192419+Ksdsk@users.noreply.github.com>
2020-09-02 20:39:53 -0300

    Update socialmedia.js

--- commands/socialmedia.js
+++ commands/socialmedia.js
@@ -8,20 +8,115 @@ module.exports = {
     description: 'This command uploads social media of the user given each argument',
     execute(message, args){
 
-            async function firstSecon() {
+
+
+            var instaUser;
+            var scUser;
+            var linkedinUser;
+            var hpUser;
+            var tiktokUser;
+            var YouTubeUser;
+
+            async function instaq() {
+
                 message.channel.send("Enter your name");
                 let filter = m => (m.author.id === message.author.id);
                 try {
                     await message.channel.awaitMessages(filter, { max: 1, time: '10000', errors: ['time'] }).then(collected =>
                         {
-                            message.channel.send("Your name is " + collected.first().content);
+                            instaUser = collected.first().content
                         });
 
                 }
                 catch(ex) {
-                    message.channel.send("You did not specify a name on time.");
+                    message.channel.send("You did not specify your username on time");
                 }
             }
-            firstSecon();
+
+
+            async function scq() {
+
+                message.channel.send("Enter your name");
+                let filter = m => (m.author.id === message.author.id);
+                try {
+                    await message.channel.awaitMessages(filter, { max: 1, time: '10000', errors: ['time'] }).then(collected =>
+                        {
+                            scUser = collected.first().content
+                        });
+
+                }
+                catch(ex) {
+                    message.channel.send("You did not specify your username on time");
+                }
+            }
+            async function linkedinq() {
+
+                message.channel.send("Enter your name");
+                let filter = m => (m.author.id === message.author.id);
+                try {
+                    await message.channel.awaitMessages(filter, { max: 1, time: '10000', errors: ['time'] }).then(collected =>
+                        {
+                            linkedinUser = collected.first().content
+                        });
+
+                }
+                catch(ex) {
+                    message.channel.send("You did not specify your username on time");
+                }
+            }
+            async function housepartyq() {
+
+                message.channel.send("Enter your name");
+                let filter = m => (m.author.id === message.author.id);
+                try {
+                    await message.channel.awaitMessages(filter, { max: 1, time: '10000', errors: ['time'] }).then(collected =>
+                        {
+                            hpUser = collected.first().content
+                        });
+
+                }
+                catch(ex) {
+                    message.channel.send("You did not specify your username on time");
+                }
+            }
+
+            async function tiktokq() {
+
+                message.channel.send("Enter your name");
+                let filter = m => (m.author.id === message.author.id);
+                try {
+                    await message.channel.awaitMessages(filter, { max: 1, time: '10000', errors: ['time'] }).then(collected =>
+                        {
+                            tiktokUser = collected.first().content
+                        });
+
+                }
+                catch(ex) {
+                    message.channel.send("You did not specify your username on time");
+                }
+            }
+
+            async function ytq() {
+
+                message.channel.send("Enter your name");
+                let filter = m => (m.author.id === message.author.id);
+                try {
+                    await message.channel.awaitMessages(filter, { max: 1, time: '10000', errors: ['time'] }).then(collected =>
+                        {
+                            YouTubeUser = collected.first().content
+                        });
+
+                }
+                catch(ex) {
+                    message.channel.send("You did not specify your username on time");
+                }
+            }
+            instaq();
+            scq();
+            tiktokq();
+            ytq();
+            housepartyq();
+            linkedinq();
+
         }
 }
