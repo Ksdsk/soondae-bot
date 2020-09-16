614cb0721abd061228bb3ae9cc7cf622a1e1325a
Ksdsk <48192419+Ksdsk@users.noreply.github.com>
2020-09-15 22:35:23 -0300

    Update therapy.js

--- commands/therapy.js
+++ commands/therapy.js
@@ -15,12 +15,30 @@ module.exports = {
         .setDescription("If the problem persists, please refer to **snd help therapy**")
         .setFooter("Treating " + message.author.username, message.author.displayAvatarURL())
 
+
+        
         let therapyStuff = ["I see...", "It must've been tough...", "I think about that too...", "I know...", "It will be alright...", "It's fine...", "*nods*", "Very hard times... I see", "Just keep going..!", "You're doing good..!", "You will be fine.", "I am proud of you.", "I see... I see", "It often does...", "Yes... definitely", "You will get through this."]
 
             async function therapyTime() {
 
-                message.channel.send(therapyStuff[Math.floor(Math.random() * 15)]);
+                message.reply(`Hey ${message.author.username}, tell me, what's been happening?`);
                 let filter = m => (m.author.id === message.author.id);
+                try {
+                    await message.channel.awaitMessages(filter, { max: 1, time: 120000, errors: ['cancel'] }).then(collected =>
+                        {
+                        });
+                        await message.channel.messages.fetch({ limit: 2 }).then(messages => { // Fetches the messages
+                            message.channel.bulkDelete(messages // Bulk deletes all messages that have been fetched and are not older than 14 days (due to the Discord API)
+                        )});
+
+                }
+                catch(ex) {
+                        message.channel.send(errorEmbed)
+                    return;
+                }
+
+                message.channel.send(therapyStuff[Math.floor(Math.random() * 15)]);
+                filter = m => (m.author.id === message.author.id);
                 try {
                     await message.channel.awaitMessages(filter, { max: 1, time: 120000, errors: ['cancel'] }).then(collected =>
                         {
@@ -71,7 +89,7 @@ module.exports = {
 
             }
 
-            message.reply(`Hey ${message.author.username}, tell me, what's been happening?`);
+            
             therapyTime();
 
     }
