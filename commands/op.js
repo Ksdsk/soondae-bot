4fcac7c02f14be99690d310ca061529a8777c6d4
Ksdsk <48192419+Ksdsk@users.noreply.github.com>
2021-02-23 17:42:43 -0400

    Update op.js

--- commands/op.js
+++ commands/op.js
@@ -1,13 +1,13 @@
-var pos = ["You will be fine.", "You can do it.", "You will succeed.", "Better days are already coming.",
-"Everything will turn out fine.", "Follow your dream.", "Go for it.", "Chase your passion.", "You will be safe."]
-
-var omn = ["You have no choice.", "You cannot stop it.", "It is inevitable.", "It is too late now", "They always do", 
-"Neverendingly.", "Now come with us"]
-
 module.exports = {
     name: 'op',
     description: 'This command creates an ominously positive text',
     execute(message, args){
+
+        var pos = ["You will be fine.", "You can do it.", "You will succeed.", "Better days are already coming.",
+"Everything will turn out fine.", "Follow your dream.", "Go for it.", "Chase your passion.", "You will be safe."]
+
+var omn = ["You have no choice.", "You cannot stop it.", "It is inevitable.", "It is too late now", "They always do", 
+"Neverendingly.", "Now come with us"]
 
         const rpos = Math.floor(Math.random() * pos.length);
         const romn = Math.floor(Math.random() * omn.length);
