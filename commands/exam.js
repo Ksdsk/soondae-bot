aeca1c038c1c759974574ef634ef07a90bfffca5
Ksdsk <soonmoc.kang@gmail.com>
2021-10-01 17:40:17 -0300

    Update exam.js

--- commands/exam.js
+++ commands/exam.js
@@ -8,24 +8,31 @@ module.exports = {
         let courseName = args.join('');
         courseName = courseName.toUpperCase();
         const domain = courseName.substring(0,4);
-        const coursecode = courseName.substring(4);
-        parsedjson = json.parse(examjson);
-        for (var section in parsedjson[domain].code[coursecode]) {
-            message.channel.send(section["number"]);
+        const coursecode = parseInt(courseName.substring(4));
+        var details = '';
+        var timejson;
+        for (var i = 0; i < myObj[domain].length; i++) {
+            console.log(i);
+            if (myObj[domain][i]["code"] == coursecode) {
+                for (var j = 0; j < myObj[domain][i]["section"].length; j++) {
+                    const toUnix = new Date(myObj[domain][i]["section"][j]["number"] + "', value: '" + myObj[domain][i]["section"][j]["time"]).getTime() - (3600 * 3);
+                    const dateObj = new Date(toUnix * 1000);
+                    
+                    details = details + "{name: 'Section ' + " + dateObj.toLocaleString('en-CA', { timeZone: 'UTC' }) + "'},"
+                }
+                details = details.substring(0, details.length - 1);
+                timejson = JSON.parse(details);
+                break;
+            }
         }
 
-        // examjson[domain].code[coursecode]
-
-        // const examEmbed = new Discord.MessageEmbed()
-        //   .setTimestamp()
-        //   .setColor('#7aff81')
-        //   .setTitle(`Exam information for ` + courseName)
-        //   .setDescription('Fall 2021/2022')
-        //   .addFields(
-
-        //       { name: 'Section ' + examjson.[domain]., value: ''},
-        //   )
-        //   .setFooter("Exam info for " + courseName + " requested by " + message.author.username, message.author.displayAvatarURL())
+        const examEmbed = new Discord.MessageEmbed()
+          .setTimestamp()
+          .setColor('#7aff81')
+          .setTitle(`Exam information for ` + courseName)
+          .setDescription('Fall 2021/2022')
+          .addFields(timejson)
+          .setFooter("Exam info for " + courseName + " requested by " + message.author.username, message.author.displayAvatarURL())
         
     }
 }
