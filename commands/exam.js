ef07828be37f32d15d255659039917725c5bbdcd
Ksdsk <soonmoc.kang@gmail.com>
2021-10-02 02:03:10 -0300

    Update exam.js

--- commands/exam.js
+++ commands/exam.js
@@ -9,31 +9,27 @@ module.exports = {
         courseName = courseName.toUpperCase();
         const domain = courseName.substring(0,4);
         const coursecode = parseInt(courseName.substring(4));
-        var details = '';
-        var timejson;
+
+        var examEmbed = new Discord.MessageEmbed()
+        .setTimestamp()
+        .setColor('#7aff81')
+        .setTitle(`Exam information for ` + courseName)
+        .setDescription('Fall 2021/2022')
+        .setFooter("Exam info for " + courseName + " requested by " + message.author.username, message.author.displayAvatarURL())
+      
         for (var i = 0; i < examjson[domain].length; i++) {
-            console.log(i);
             if (examjson[domain][i]["code"] == coursecode) {
                 for (var j = 0; j < examjson[domain][i]["section"].length; j++) {
                     const toUnix = new Date(examjson[domain][i]["section"][j]["number"] + "', value: '" + examjson[domain][i]["section"][j]["time"]).getTime() - (3600 * 3);
                     const dateObj = new Date(toUnix * 1000);
-                    
-                    details = details + "{name: 'Section ' + " + dateObj.toLocaleString('en-CA', { timeZone: 'UTC' }) + "'},"
+                    examEmbed.addFields({name: 'Section ' + dateObj.toLocaleString('en-CA', { timeZone: 'UTC' })});
                 }
                 details = details.substring(0, details.length - 1);
-                timejson = JSON.parse(details);
                 break;
-            }
+            } 
         }
 
-        const examEmbed = new Discord.MessageEmbed()
-          .setTimestamp()
-          .setColor('#7aff81')
-          .setTitle(`Exam information for ` + courseName)
-          .setDescription('Fall 2021/2022')
-          .addFields(timejson)
-          .setFooter("Exam info for " + courseName + " requested by " + message.author.username, message.author.displayAvatarURL())
-        
+
         message.channel.send(examEmbed);
     }
 }
